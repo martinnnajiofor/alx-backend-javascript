d063e4ff7da380c86090dfe1ffbc7f730fd6ae12
martinnnajiofor <martinnnajiofor@yahoo.com>
2023-01-23 05:35:46 -0800

    0x00-ES6_basic

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,4 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employees = [];
-
-  for (const employee of reportWithIterator) {
-    employees.push(employee);
-  }
-
-  return employees.join(' | ');
+	return reportWithIterator.join(' | ');
 }
 
